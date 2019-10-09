a06299cad1f81b034272fe38ecdb6ed6cffe5872
areong9292 <areong9292@gmail.com>
2019-10-09 13:22:40 +0900

    SourceTree Test - Git auth problem

--- DesignPattern/DesignPattern.cpp
+++ DesignPattern/DesignPattern.cpp
@@ -2,24 +2,6 @@
 #include "Component.h"
 int main()
 {
-	Component* ss1;
-	Component* ss2;
-
-	ss1 = Component::GetInstance();
-
-	ss1->setVal(2);
-	ss1->test();
-
-	ss2 = Component::GetInstance();
-	ss2->test();
-	ss2->setVal(3);
-
-	ss1->test();
-
-	std::cout << ss1 << std::endl;
-	std::cout << ss2 << std::endl;
-
-	Component::GetInstance()->test();
-
+	
 	return 0;
 }
